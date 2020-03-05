ed318e2c29e71daa413e802bd0272a6877a3fecc
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-03-05 14:18:17 -0800

    made changes to add dummy data and added models & a category file

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,15 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMeals from '../screens/CategoryMealsScreen'
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
 
-createStackNavigator({
-    
+const MealsNavigator = createStackNavigator({
+    Categories: CategoriesScreen,
+    CategoryMeals: {
+        screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
 });
+
+export default createAppContainer(MealsNavigator);
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import CategoryMealsScreen from './CategoryMealsScreen';
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity 
+                style={styles.gridItem}
+                onPress={() => {
+                    props.navigation.navigate({routeName: 'CategoryMeals'});
+                }}
+            >
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+    console.log(props);
     return(
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-        </View>
+        <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={ CATEGORIES } 
+        renderItem={renderGridItem} 
+        numColumns={2} 
+        />
     );
 };
 
@@ -14,6 +34,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
     }
 });
 
